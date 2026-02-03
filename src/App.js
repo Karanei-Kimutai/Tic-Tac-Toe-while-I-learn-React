f100867312980b2cb2b0df92b1ccb2ca473756dd
Karanei-Kimutai <karaneijunior@gmail.com>
2026-02-03 10:22:44 +0300

    implemented the handle play function

--- src/App.js
+++ src/App.js
@@ -44,7 +44,8 @@ export default function Game(){
   const [history, setHistory]= useState([Array(9).fill(null)]);
   const currentSquares=history[history.length -1];
   function handlePlay(nextSquares){
-    //TODO
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
   }
   return(
     <div className="game">
